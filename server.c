d5b043061ee9fcc98f42505a738383dcb3c35a45
Onyx <info@jeremy.best>
2022-03-28 00:50:41 +0200

    1

--- server.c
+++ server.c
@@ -10,12 +10,36 @@
 #include <bits/socket.h>
 #include <fcntl.h>
 
+#include <signal.h>
 
 #define MAX_CONN 4000
 #define PORT 5454
 #define TIMEOUT 400
 #define BUFF_SIZE 255
-#define 
+
+int cleanup = 1;
+
+struct	s_cert_file {
+	char *server_cert;
+	char *server_key;
+};
+
+struct	s_socket {
+	int sockfd;
+	WOLFSSL *ssl;
+};
+
+void	ft_fail(char *str)
+{
+	char error[255];
+
+	strcpy(error, "[ERROR]\terror to ");
+	strncpy(error, str, 200);
+	strcpy(error, " : ");
+	perror(error);
+
+	exit(EXIT_FAILURE);
+}
 
 int	set_nonblocking(int fd)
 {
@@ -28,7 +52,7 @@ void	set_sockaddr(struct sockaddr_in *sockaddr)
 {
 	memset(sockaddr, 0, sizeof(struct sockaddr_in));
 	sockaddr->sin_family = AF_INET;
-	sockaddr->sin_addr.s_addr = htonl(INADR_ANY);
+	sockaddr->sin_addr.s_addr = htonl(INADDR_ANY);
 	sockaddr->sin_port = htons(PORT);
 }
 
@@ -42,13 +66,12 @@ int	init_socket(int *sockfd_back)
 	sockfd = socket(PF_INET, SOCK_STREAM, 0);
 
 	if(sockfd < 0)
-		ft_fail();
+		ft_fail("sockfd create");
 	
 	yes = 1;
 
 	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0)
 		return (-1);
-
 
 	set_sockaddr(&host_addr);
 
@@ -64,33 +87,85 @@ int	init_socket(int *sockfd_back)
 	return (0);
 }
 
-void	*handle_smtp(void *smtp_arg)
+void	*handle_sock(void *smtp_arg)
 {
+	struct s_socket *socket;
 	int sockfd;
-	sockfd = *(int *)smtp_arg;
+	int err;
+	WOLFSSL *ssl;
+
+	socket = (struct s_socket *)smtp_arg;
+	sockfd = socket->sockfd;
+	ssl = socket->ssl;
+
+	int n;
+	char buf[BUFF_SIZE + 1];
+
+	n = wolfSSL_accept(ssl);
+	if(n != SSL_SUCCESS)
+	{
+		err = wolfSSL_get_error(ssl, n);
+		fprintf(stderr, "[ERROR]\tsocket failed to handshake error => %d string => %s\n", n, wolfSSL_ERR_error_string(err, buf));
+	}
+
+	n = 0;
+	while((n = wolfSSL_read(ssl, buf, BUFF_SIZE)) > 0)
+	{
+		if(wolfSSL_write(ssl, buf, n) != n)
+			fprintf(stderr, "[ERROR]\tsocket failed to write\n");
+	}
+
+	if(n < 0)
+	{
+		err = wolfSSL_get_error(ssl, n);
+		fprintf(stderr, "[ERROR]\tsocket failed to read error => %d string => %s\n", n, wolfSSL_ERR_error_string(err, buf));
+	}
+
+
 	free(smtp_arg);
 	
+	wolfSSL_free(ssl);
 	close(sockfd);
+
 	pthread_exit((void *)EXIT_SUCCESS);
+}
+
+void	sig_handler(int sig)
+{
+	printf("[INFO]\tSIGINT handled \t");
+	cleanup = 0;
 }
 
 int	main(int argc, char **argv)
 {
+	struct sigaction act;
+	act.sa_handler = sig_handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGINT, &act, NULL);
+
 	int sockfd;
 	int new_sockfd;
-	int *thread_arg;
+	struct s_socket *thread_arg;
 	char buf[BUFF_SIZE + 1];
 
-	struct scokaddr_in client_addr;
+	struct sockaddr_in client_addr;
 	socklen_t sin_size;
+	
 	pthread_t last_thread;
 	
+
+	struct s_cert_file cert_file;
+
+	cert_file.server_cert	= "./certs/certificate.pem";
+	cert_file.server_key 	= "./certs/key.pem";
+
 	WOLFSSL_METHOD *method;
 	WOLFSSL_CTX *ctx;
 	WOLFSSL *ssl;
 
-	if(init_sockfd(&sockfd) < 0)
-		ft_fail();
+	if(init_socket(&sockfd) < 0)
+		ft_fail("init socket");
 	
 	sin_size = sizeof(struct sockaddr_in);
 
@@ -101,28 +176,36 @@ int	main(int argc, char **argv)
 	ctx = wolfSSL_CTX_new(method);
 
 	if(ctx == NULL)
-		ft_fail();
+		ft_fail("create ctx wolfssl");
 
-	if(wolfSSL_CTX_load_verify_location(ctx, , 0) != SSL_SUCCESS)
-		ft_fail();
+	if(wolfSSL_CTX_use_certificate_file(ctx, cert_file.server_cert, SSL_FILETYPE_PEM) != SSL_SUCCESS)
+		ft_fail("import server_cert");
 	
+	if(wolfSSL_CTX_use_PrivateKey_file(ctx, cert_file.server_key, SSL_FILETYPE_PEM) != SSL_SUCCESS)
+		ft_fail("import server_key");
 
 
-
-	while(1)
+	while(cleanup)
 	{
 		new_sockfd = accept(sockfd, (struct sockaddr*)&client_addr, &sin_size);
-		if(new_socfd < 0)
+		set_nonblocking(new_sockfd);
+		if(new_sockfd < 0)
 		{
 			printf("[ERROR]\tError to accept connexion for socket => %d\n", new_sockfd);
 			continue;
 		}
 
-		buf = inet_ntoa((struct in_addr)client_addr.sin_addr);
+		inet_ntop(AF_INET, &(client_addr.sin_addr), buf, sin_size);
 		printf("[INFO]\tnew connexion %s:%d\n", buf, ntohs(client_addr.sin_port));
 		
-		thread = (int *)malloc(sizeof(int));
-		*thread = new_sockfd;
+		thread_arg = (struct s_socket *)malloc(sizeof(struct s_socket));
+		thread_arg->sockfd = new_sockfd;
+		thread_arg->ssl = wolfSSL_new(ctx);
+
+		if(thread_arg->ssl == NULL)
+			continue;
+
+		wolfSSL_set_fd(ssl, sockfd);
 		
 		if(pthread_create(&last_thread, NULL, &handle_sock, (void *)thread_arg) < 0)
 		{
@@ -131,6 +214,9 @@ int	main(int argc, char **argv)
 		}
 		
 	}
+
+	wolfSSL_CTX_free(ctx);
+	wolfSSL_Cleanup();
 
 	close(sockfd);
 
---
+++ test.c
@@ -0,0 +1,60 @@
+#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+#include <wolfssl/test.h>
+#include <errno.h>
+#define SERV_PORT 11111
+#define MAX_LINE 4096
+
+int main(int argc, char** argv)
+{
+int listenfd, connfd;
+WOLFSSL_CTX* ctx;
+WOLFSSL* ssl;
+int n;
+char buf[MAX_LINE];
+WOLFSSL_METHOD* method;
+
+    /* Initialize wolfSSL library */
+wolfSSL_Init();
+
+    /* Get encryption method */
+method = wolfTLSv1_2_server_method();
+
+    /* Create wolfSSL_CTX */
+if ( (ctx = wolfSSL_CTX_new(method)) == NULL)
+     err_sys("wolfSSL_CTX_new error");
+
+    /* Load server certs into ctx */
+if (wolfSSL_CTX_use_certificate_file(ctx, "certs/server-cert.pem",
+SSL_FILETYPE_PEM) != SSL_SUCCESS)
+     err_sys("Error loading certs/server-cert.pem");
+
+    /* Load server key into ctx */
+if (wolfSSL_CTX_use_PrivateKey_file(ctx, "certs/server-key.pem",
+SSL_FILETYPE_PEM) != SSL_SUCCESS)
+     err_sys("Error loading certs/server-key.pem");
+
+tcp_accept(&listenfd, &connfd, NULL, SERV_PORT, 0, 0, 0);
+
+    /* Create wolfSSL object */
+if ( (ssl = wolfSSL_new(ctx)) == NULL)
+     err_sys("wolfSSL_new error");
+
+    wolfSSL_set_fd(ssl, connfd);
+
+    if ( (n = wolfSSL_read(ssl, buf, (sizeof(buf) -1))) > 0) {
+printf("%s\n", buf);
+if (wolfSSL_write(ssl, buf, n) != n)
+     err_sys("wolfSSL_write error");
+}
+if (n <0)
+        printf("wolfSSL_read error = %d\n", wolfSSL_get_error(ssl, n));
+else if (n == 0)
+     printf("Connection close by peer\n");
+
+    wolfSSL_free(ssl);
+close(connfd);
+wolfSSL_CTX_free(ctx);
+wolfSSL_Cleanup();
+exit(EXIT_SUCCESS);
+}
